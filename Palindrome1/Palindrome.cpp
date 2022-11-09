0ce870167547e61b589524fd1a4cd957a2335541
Gianluca Caregnato <gianluca.caregnato@studenti.unipd.it>
2022-11-09 09:35:11 +0100

    Palindrome

---
+++ Palindrome1/Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//#include <string>
+
+
+bool is_palindrome(const std::string& s);
+
+int main() {
+
+	std::string test = "ciao";
+
+	std::cout << test[1];
+
+}
+
+bool is_palindrome(const std::string& s) {
+
+	
+
+}
