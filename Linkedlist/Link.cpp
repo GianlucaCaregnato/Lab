e3d0991c220dda2e1968a868568aa754ff96f4a7
Gianluca Caregnato <gianluca.caregnato@studenti.unipd.it>
2022-11-09 12:41:42 +0100

    Link

--- Linkedlist/Link.cpp
+++ Linkedlist/Link.cpp
@@ -1,6 +1,6 @@
 #include "Link.h"
 
-Link::Link(const std::string& v, Link* p, Link* s) :value{ v }, prev{ p }, succ{ s } {}
+Link::Link(const std::string& v , Link* p = nullptr, Link* s = nullptr) :value{v}, prev{p}, succ{s} {}
 
 Link* Link::insert(Link n) {//inserisco n prima del nodo corrente
 
@@ -11,5 +11,50 @@ Link* Link::insert(Link n) {//inserisco n prima del nodo corrente
 
 	return this;
 }
+
+Link* Link::add(Link n) {
+
+	n.prev = this;
+	n.succ = this->succ;
+
+	this->succ = &n;
+
+	return this;
+}
+
+Link* Link::erase()
+{
+	this->prev->succ = this->succ;
+	this->succ->prev = this->prev;
+
+	return this;
+}
+
+Link* Link::find(std::string v)
+{
+	if (this->value == v) {
+		return this;
+	}
+
+
+	Link n = *this;
+	while (n.succ != nullptr) {
+		n = *n.succ;
+		if (n.value == v) {
+			return &n;
+		}
+	}
+
+	n = *this;
+	while (n.prev != nullptr) {
+		n = *n.prev;
+		if (n.value == v) {
+			return &n;
+		}
+	}
+
+	return nullptr;
+}
+
 
 
